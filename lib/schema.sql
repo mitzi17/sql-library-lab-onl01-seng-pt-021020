459eb6c78f057535a547a829af0ae946ca39bb5f
Mitzi Estrada <m_estrada17@live.com>
2020-04-13 17:42:49 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,27 @@ CREATE TABLE series (
   author TEXT
   sub_genre TEXT
 );
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT
+  series TEXT
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+  motto TEXT
+  species TEXT
+  author TEXT
+);
