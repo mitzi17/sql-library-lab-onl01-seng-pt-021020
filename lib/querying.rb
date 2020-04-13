ce374a82f12a76f05614dd6deefdfc9111643594
Mitzi Estrada <m_estrada17@live.com>
2020-04-13 18:47:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,8 +33,8 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN books
   ON series.id = books.series_id
-  JOIN characters
-  ON "
+  JOIN character_books
+  ON books.id = character_books.book_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
