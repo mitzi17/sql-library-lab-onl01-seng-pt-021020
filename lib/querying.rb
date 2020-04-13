581676585e74a4e0d90cfdff3b24b2f1090c271a
Mitzi Estrada <m_estrada17@live.com>
2020-04-13 18:28:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(*)
-  FROM characters;"
+  FROM characters
+  GROUP BY species;"
 end
 
 def select_name_and_series_subgenres_of_authors
